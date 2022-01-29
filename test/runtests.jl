eb55b0e7b196a0700a524a246f710056b9a93128
WhiffleFish <tylertronics@gmail.com>
2022-01-29 16:27:02 -0700

    simple inference test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,24 @@
 using DeepCFR
 using Test
+using CounterfactualRegret
+
+const CFR = CounterfactualRegret
+using StaticArrays
+
+function DeepCFR.vectorized(game::CFR.Kuhn, I)
+    p, pc, hist = I
+    L = length(hist)
+    h = Tuple(hist)
+    leftover = ntuple(_ -> -1, 2 - L)
+    SA[p, pc, h..., leftover...]::SVector{4,Int}
+end
 
 @testset "DeepCFR.jl" begin
-    # Write your tests here.
+    game = CFR.Kuhn()
+    sol = DeepCFRSolver(game)
+    train!(sol, 10)
+    I0 = infokey(game, initialhist(game))
+    σ = @inferred sol(DeepCFR.vectorized(game, I0))
+    @test σ isa Vector{<:AbstractFloat}
+    @test sum(σ) ≈ 1.0
 end
