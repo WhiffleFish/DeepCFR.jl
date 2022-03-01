e28649f90605880ab8b02a929e3e67d59cb24d30
WhiffleFish <tylertronics@gmail.com>
2022-02-28 20:17:16 -0700

    callbacks

--- src/DeepCFR.jl
+++ src/DeepCFR.jl
@@ -5,11 +5,13 @@ using Random
 using ProgressMeter
 using Base.Iterators
 using Flux
+using FileIO
 
 include("memory.jl")
 include("solver.jl")
 include("ESCFR.jl")
 include("train.jl")
+include("callback.jl")
 
 export DeepCFRSolver
 
---
+++ src/callback.jl
@@ -0,0 +1,33 @@
+mutable struct Throttle{F}
+    f::F
+    n::Int
+    state::Int
+end
+
+function throttle(f::Function, n::Int)
+    return Throttle(f, n, 0)
+end
+
+function (t::Throttle)()
+    iszero(rem(t.state, t.n)) && t.f()
+    t.state += 1
+end
+
+mutable struct ModelSaver{M}
+    model::M
+    n::Int
+    path::String
+    state::Int
+end
+
+function ModelSaver(model, n::Int=500; path=pwd())
+    return ModelSaver(model, n, path, 0)
+end
+
+function (m::ModelSaver)()
+    if iszero(rem(m.state, m.n))
+        path = joinpath(m.path, "model_$(m.state).jld2")
+        FileIO.save(path, Dict("model" => m.model))
+    end
+    m.state += 1
+end
